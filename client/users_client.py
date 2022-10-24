2310be55ee7bd891fb1b0d2e5a0af92b4beb7b0c
Jere Pfluger <jerepfluger@gmail.com>
2022-10-24 15:15:05 +0200

    adding redis client functionality (#3)
    Co-authored-by: Jeremias Pfluger <jeremiaspfluger@Jeremias-Pfluger.local>

--- client/users_client.py
+++ client/users_client.py
@@ -1,12 +1,10 @@
-import requests
-
 from config.config import settings as config_file
 from domain.mock_requests import MockRequests
 
 
 class UsersClient:
     def __init__(self):
-        self.base_url = config_file.users.url
+        self.base_url = config_file.users.host
 
     def get_user(self, email):
         url = f'{self.base_url}/user?email={email}'
--- service/redis_service.py
+++ service/redis_service.py
@@ -1,3 +1,4 @@
+import json
 import uuid
 
 from client.redis_client import RedisClient
@@ -8,8 +9,13 @@ class RedisService:
     def __init__(self):
         self.redis_client = RedisClient()
 
-    def save_info(self, object_to_store):
+    def save_info(self, object_to_store, expiry=None):
         uu_id = uuid.uuid4()
-        logger.info(f'Saving object to redis with Key {uu_id} and Value {object_to_store}')
+        logger.info(f'Saving object to redis with Key {uu_id}')
+        self.redis_client.save_key_value(str(uu_id), json.dumps(object_to_store), expiry)
 
         return uu_id
+
+    def get_info(self, key):
+        logger.info(f'Retrieving redis info for Key {key}')
+        return self.redis_client.get_value(key)
